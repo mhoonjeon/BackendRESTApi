38f1fa1f30f1a30df088227352e8494e07f36480
mhjeon <mhjeon@modernlab.io>
2018-08-07 20:38:11 +0900

    MED-66 Refactor models: trivial

--- BackendRESTApi/sentences/models.py
+++ BackendRESTApi/sentences/models.py
@@ -1,10 +1,11 @@
 from model_utils.models import TimeStampedModel
-from uuid
+import uuid
 
 from django.db import models
 
 
 class Sentence(TimeStampedModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    charts = models.ForeignKey("charts.Chart", related_name="sentences")
+    charts = models.ForeignKey("charts.Chart", on_delete=models.CASCADE,
+                               related_name="sentences")
     content = models.CharField(max_length=256)
