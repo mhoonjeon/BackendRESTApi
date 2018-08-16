bc3421da5f8c685adb51c8a92e80b8f0de3099e9
mhjeon <mhjeon@modernlab.io>
2018-08-16 18:39:56 +0700

    MED-66 Simple websocket implementation for frontend testing
    Frontend can test websocket messages without deep learning module
    dependency

--- BackendRESTApi/transcriptions/consumers.py
+++ BackendRESTApi/transcriptions/consumers.py
@@ -29,7 +29,8 @@ class ClientConsumer(AsyncWebsocketConsumer):
         self.patient_id = self.scope['path'].split('/')[-2]
         self.patient_group_name = '{patient_id}_group'.format(patient_id=self.patient_id)
 
-        # # Start deep learning module
+        # Start deep learning module
+        #TODO: need to confirm websocket with deep before allowing frontend connection
         await self._start_deep_engine(self.patient_id)
 
         # Join channel 'group' where messages from deep learning module and
@@ -50,75 +51,9 @@ class ClientConsumer(AsyncWebsocketConsumer):
 
     # Receive message from WebSocket(Client)
     async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        raw_sentence = text_data_json['raw_sentence']
-
-        # Send message to room group
-        await self.channel_layer.group_send(
-            self.patient_group_name,
-            {
-                'type': 'propagate.input.to.deep',
-                'raw_sentence': raw_sentence
-            }
-        )
-
-    # Receive result from channel group
-    async def propagate_result_to_client(self, event):
-        result = event['result']
-
-        # Send result to client
-        await self.send(text_data=json.dumps(result))
-
-    async def propagate_input_to_deep(self, event):
-        pass
-
-
-class DeepConsumer(AsyncWebsocketConsumer):
-
-    async def connect(self):
-        self.patient_id = self.scope['path'].split('/')[-2]
-        self.patient_group_name = '{patient_id}_group'.format(patient_id=self.patient_id)
-
-        # Join channel 'group' where messages from deep learning module and
-        # client can be shared
-        await self.channel_layer.group_add(
-            self.patient_group_name,
-            self.channel_name
-        )
-
-        await self.accept()
-
-    async def disconnect(self, close_code):
-        # Leave channel group
-        await self.channel_layer.group_discard(
-            self.patient_group_name,
-            self.channel_name
-        )
-
-    # Receive message from WebSocket(Deep learning module)
-    async def receive(self, text_data):
-        # TODO: change after integration with deep engine
         text_data = RESPONSE_FROM_DEEP
 
         text_data_json = json.dumps(text_data)
         result = text_data_json
 
-        await self.channel_layer.group_send(
-            self.patient_group_name,
-            {
-                'type': 'propagate.result.to.client',
-                'result': result
-            }
-        )
-
-    # Receive raw sentence from client group
-    async def propagate_input_to_deep(self, event):
-        raw_sentence = event['raw_sentence']
-
-        # Send raw sentence to deep engine
-        await self.send(text_data=json.dumps({
-            'raw_sentence': raw_sentence
-        }))
-
-    async def propagate_result_to_client(self, event):
-        pass
+        await self.send(text_data=json.dumps(result))
--- BackendRESTApi/transcriptions/tests-original.py
+++ BackendRESTApi/transcriptions/tests-original.py
@@ -4,11 +4,11 @@ import os
 import pytest
 import websocket
 from channels.testing import WebsocketCommunicator
-from .consumers import ClientConsumer
+from .consumers import ClientConsumer, DeepConsumer
 
 
 @pytest.mark.asyncio
-async def test_frontend_can_test_temporarily_before_deep_integration():
+async def test_client_consumer_can_connect():
     patient_id = 'abc123'
     client_URL = f'/ws/v1/transcriptions/client/{patient_id}/'
     client_communicator = WebsocketCommunicator(ClientConsumer, client_URL)
@@ -19,10 +19,26 @@ async def test_frontend_can_test_temporarily_before_deep_integration():
         "raw_sentence": "My stomach aches",
     }
 
+    # Suppose deep learning module is connected
+    deep_URL = f'/ws/v1/transcriptions/deep/{patient_id}/'
+    deep_communicator = WebsocketCommunicator(DeepConsumer, deep_URL)
+    connected, subprotocol = await deep_communicator.connect()
+    assert connected
+
+
     # client send text to deep learning and check if deep engine got it
     await client_communicator.send_to(text_data=json.dumps(text_data))
+    response = await deep_communicator.receive_from()
+    response = json.loads(response)
+    assert 'My stomach aches' == response['raw_sentence']
+
+    # deep engine send result back to client
+    # TODO: need to be changed after integration with deep engine
+    text_data = {'trigger': 'temp'}
+    await deep_communicator.send_to(text_data=json.dumps(text_data))
     response = await client_communicator.receive_from()
     response = json.loads(response)
     assert 'deep_output' in response
 
     await client_communicator.disconnect()
+    await deep_communicator.disconnect()
