c322326fbf2b46a026417dd20e8b6a337dddeaf1
mhjeon <mhjeon@modernlab.io>
2018-08-16 18:20:18 +0700

    MED-66 Configure routing for transcriptions module

--- BackendRESTApi/routing.py
+++ BackendRESTApi/routing.py
@@ -2,12 +2,14 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chat.routing
+import BackendRESTApi.transcriptions.routing
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
     'websocket': AuthMiddlewareStack(
         URLRouter(
-            chat.routing.websocket_urlpatterns
+            chat.routing.websocket_urlpatterns + \
+            BackendRESTApi.transcriptions.routing.websocket_urlpatterns
         )
     ),
 })
