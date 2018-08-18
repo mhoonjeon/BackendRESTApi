e92c9e7a39cdf41eeb07fec5843a576b00bc127b
mhjeon <mhjeon@modernlab.io>
2018-08-18 17:17:00 +0700

    MED-74 Separate PatientSerializer for GET and POST methods
    To display gender field in human-readable form

--- BackendRESTApi/patients/serializers.py
+++ BackendRESTApi/patients/serializers.py
@@ -3,7 +3,15 @@ from .models import Patient
 
 
 class PatientSerializer(serializers.ModelSerializer):
+    gender = serializers.CharField(source='get_gender_display')
 
+    class Meta:
+        model = Patient
+        fields = ('id', 'name', 'gender', 'age',)
+        read_only_fields = ('gender', )
+
+
+class CreatePatientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Patient
         fields = ('id', 'name', 'gender', 'age',)
