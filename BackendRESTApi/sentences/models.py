21fd079e5b0182428dcdd5aa8cfb655c1bddf5e4
mhjeon <mhjeon@modernlab.io>
2018-08-19 17:03:30 +0700

    MED-75 Create Sentence model

--- BackendRESTApi/sentences/models.py
+++ BackendRESTApi/sentences/models.py
@@ -7,20 +7,19 @@ from model_utils.models import TimeStampedModel
 from django.db import models
 
 CATEGORIES = Choices(
-    (0, 'CC', _('Chief Complaint')),
-    (1, 'PI', _('Present Illness')),
-    (2, 'PMH', _('Past Medical History')),
-    (3, 'FH', _('Family History')),
-    (4, 'SH', _('Social History')),
-    (5, 'ROS', _('Review of System'))
+    ('CC', 'Chief Complaint'),
+    ('PI', 'Present Illness'),
+    ('PMH', 'Past Medical History'),
+    ('FH', 'Family History'),
+    ('SH', 'Social History'),
+    ('ROS', 'Review of System')
 )
 
 
 class Sentence(TimeStampedModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     chart = models.ForeignKey('charts.Chart', on_delete=models.CASCADE, related_name="sentences")
+    raw = models.CharField(max_length=1024)
     category = models.CharField(max_length=5, choices=CATEGORIES, null=True)
-    raw_sentence = models.CharField(max_length=1024)
-    deep_output = models.CharField(max_length=1024)
-    final_sentence = models.CharField(max_length=1024)
+    deep_output = models.CharField(max_length=5, choices=CATEGORIES, null=True)
     # history = HistoricalRecords()
