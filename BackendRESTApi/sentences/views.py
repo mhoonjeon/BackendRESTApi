c9f36c9a81890aa9a59efc44781bdb3030b34aad
mhjeon <mhjeon@modernlab.io>
2018-08-20 11:45:27 +0900

    MED-75 Apply bulk create operation on \sentences

--- BackendRESTApi/sentences/views.py
+++ BackendRESTApi/sentences/views.py
@@ -17,3 +17,9 @@ class SentenceViewSet(mixins.CreateModelMixin,
     permission_classes = (IsAuthenticated, )
     filter_backends = (filters.DjangoFilterBackend, )
     filterset_fields = ('chart', )
+
+    def get_serializer(self, *args, **kwargs):
+        if isinstance(kwargs.get('data', {}), list):
+            kwargs['many'] = True
+        return super().get_serializer(*args, **kwargs)
+
