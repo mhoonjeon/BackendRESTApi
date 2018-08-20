99b157797844bfa1c035fa170a88c0c53224f671
mhjeon <mhjeon@modernlab.io>
2018-08-20 11:14:54 +0900

    MED-75 Create tests for sentences (factory and serializer tests)

---
+++ BackendRESTApi/sentences/test/factories.py
@@ -0,0 +1,27 @@
+import pytz
+
+import random
+
+import factory
+
+from ..models import Sentence, CATEGORIES
+from BackendRESTApi.charts.test.factories import ChartFactory
+
+
+def get_category():
+    category_choices = [x[0] for x in CATEGORIES]
+    return random.choice(category_choices)
+
+
+class SentenceFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = 'sentences.Sentence'
+
+    id = factory.Faker('uuid4')
+    chart = factory.SubFactory(ChartFactory)
+    raw = factory.Faker('sentence')
+    category = factory.LazyFunction(get_category)
+    deep_output = factory.LazyFunction(get_category)
+    created = factory.Faker('date_time', tzinfo=pytz.utc)
+    modified = factory.Faker('date_time', tzinfo=pytz.utc)
