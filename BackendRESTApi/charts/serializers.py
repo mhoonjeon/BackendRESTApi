18e721fe8a85b8df4f77b2d0314d5d1a4e8d0034
mhjeon <mhjeon@modernlab.io>
2018-08-23 19:28:51 +0900

    MED-82 Change serializer to handle multiple CC-mapped sentences

--- BackendRESTApi/charts/serializers.py
+++ BackendRESTApi/charts/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import Chart
 from BackendRESTApi.patients.serializers import PatientSerializer
+from BackendRESTApi.sentences.serializers import SentenceSerializer
+from BackendRESTApi.sentences.models import Sentence
 
 
 class CreateChartSerializer(serializers.ModelSerializer):
@@ -12,6 +14,8 @@ class CreateChartSerializer(serializers.ModelSerializer):
 
 class GetChartSerializer(serializers.ModelSerializer):
     patient_name = serializers.CharField(source='patient.name')
+    patient_id = serializers.CharField(source='patient.id')
+    patient_cc = serializers.SerializerMethodField()
     created = serializers.DateTimeField(format="%Y년 %m월 %d일 %H:%M:%S")
     modified = serializers.DateTimeField(format="%Y년 %m월 %d일 %H:%M:%S")
 
@@ -26,3 +30,11 @@ class GetChartSerializer(serializers.ModelSerializer):
         if remove_patient_name:
             self.fields.pop('patient_name', None)
             self.fields.pop('patient', None)
+
+    def get_patient_cc(self, object):
+        try:
+            # Two of more sentences are mapped to CC
+            patient_cc = object.sentences.filter(category='CC')[:1].get()
+            return patient_cc.raw
+        except Sentence.DoesNotExist:
+            return None
