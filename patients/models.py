4313097693b42b99e942c0cda79e7bd6486d6e36
mhjeon <mhjeon@modernlab.io>
2018-11-13 21:41:05 +0900

    Change patient gender field

--- patients/models.py
+++ patients/models.py
@@ -6,16 +6,9 @@ from django.db import models
 
 
 class Patient(TimeStampedModel):
-    MALE = 'male'
-    FEMALE = 'female'
-    GENDER = Choices((MALE, 'male'), (FEMALE, 'female'))
-
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
-    gender = models.CharField(
-        choices=GENDER, max_length=5,
-        default=MALE, blank=True, null=True
-    )
+    gender = models.CharField(max_length=5, blank=True, null=True)
     age = models.PositiveSmallIntegerField(blank=True, null=True)
 
     class Meta:
